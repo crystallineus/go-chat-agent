e0e327d5d5bc15a8ee62d28a50174a32c997ed68
Chien-Shan Peng <crystalline@crystalline.work>
2025-04-19 15:36:26 -0700

    init the agent

---
+++ go.mod
@@ -0,0 +1,8 @@
+module go-chat-agent
+
+go 1.23.4
+
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/sashabaranov/go-openai v1.38.2 // indirect
+)
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	openai "github.com/sashabaranov/go-openai"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	apiKey := os.Getenv("OPENAI_API_KEY")
+	client := openai.NewClient(apiKey)
+
+	resp, err := client.CreateChatCompletion(
+		context.Background(),
+		openai.ChatCompletionRequest{
+			Model: openai.GPT3Dot5Turbo,
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    "user",
+					Content: "Say hi in a fun way!",
+				},
+			},
+		},
+	)
+
+	if err != nil {
+		log.Fatalf("API error: %v\n", err)
+	}
+
+	fmt.Println(resp.Choices[0].Message.Content)
+}
